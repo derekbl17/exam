7fd9522674426fbc8ba07b82472f4beccb220901
derekbl17 <derekc1fill@gmail.com>
2026-02-15 12:23:55 +0200

    c

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json()); // enables reading of data in request body if its in .j
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'https://exam-kvrv0o2o3-derekbl17s-projects.vercel.app/',
+    origin: 'https://exam-kvrv0o2o3-derekbl17s-projects.vercel.app',
     credentials: true, // ABSOLUTELY CRITICAL for cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
